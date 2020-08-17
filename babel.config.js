47b28856a863647afdc27ad67a603698922a020e
Julmer Olivero <jolivero.03@gmail.com>
2020-08-17 15:04:18 -0400

    Fix a Bad Configuration in the last push

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = ['@babel/preset-env',{
-    "targets": {
-      "node": "current"
-    }
-  }, '@babel/preset-react'];
+  const presets = ['@babel/preset-env',
+  '@babel/preset-react'];
 
   return {
     presets
